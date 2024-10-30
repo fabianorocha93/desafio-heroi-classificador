0fd38d1fe1c6af998d38432a590f462b8a6f639f
fabianorocha93 <162823827+fabianorocha93@users.noreply.github.com>
2024-10-30 14:42:46 -0300

    desenvolvimento da solução

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Desafio DIO")
+
+let nome = "João";
+let xp = 100000;
+
+// Categorias de ranking e valores mínimos de XP necessários para cada uma
+const rankings = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "ascendente", "Imortal", "Radiante"];
+const xpMinimo = [0, 1001, 2001, 5001, 7001, 8001, 9001, 10001, 1000000]; // XP mínimo para cada categoria
+
+let categoria = rankings[0];
+
+for (let i = 0; i < xpMinimo.length; i++) {
+    if (xp >= xpMinimo[i]) {
+        categoria = rankings[i];
+    } else {
+        break; // Para o loop assim que a categoria correta é encontrada
+    }
+}
+console.log(`O Herói de nome ${nome} está no nível de ${categoria}`);
